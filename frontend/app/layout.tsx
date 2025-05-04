92df866a5dfc48b3b9483ea061fff19d3c1be8aa
TheLastHobbit <2865755738@qq.com>
2025-05-05 01:22:32 +0800

    update frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="zh-CN" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+      <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
           <WalletProvider>
             <div className="flex min-h-screen flex-col">
               <Header />
